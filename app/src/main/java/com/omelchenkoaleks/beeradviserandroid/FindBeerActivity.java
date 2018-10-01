6d7c0daeac1154a88169801a649580992c69d587
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-01 06:59:37 +0300

    В главном активити создан метод для обработки нажатия на кнопку в макете.

--- app/src/main/java/com/omelchenkoaleks/beeradviserandroid/FindBeerActivity.java
+++ app/src/main/java/com/omelchenkoaleks/beeradviserandroid/FindBeerActivity.java
@@ -2,12 +2,31 @@ package com.omelchenkoaleks.beeradviserandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends Activity {
+
+    TextView brands;
+    Spinner color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // метод сообщает какой макет используется активностью
         setContentView(R.layout.activity_find_beer);
+    }
+
+    // метод сработает, когда юзер кликнет по кнопке
+    public void onClickFindBeer(View view) {
+        // получаем ссылку на TextView
+        brands = findViewById(R.id.brands);
+        // получаем ссылку на Spinner
+        color = findViewById(R.id.color);
+        // получаем вариант, выбранный в Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+        // выводим выбранный вариант
+        brands.setText(beerType);
     }
 }
